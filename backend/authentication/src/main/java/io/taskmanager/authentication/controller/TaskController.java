e0dcba780a96093a5b55cb335e17b475b7693ec2
Vineeshakatta <vineesha.katta123@gmail.com>
2026-01-28 18:43:05 -0500

    added task controller changes

--- backend/authentication/src/main/java/io/taskmanager/authentication/controller/TaskController.java
+++ backend/authentication/src/main/java/io/taskmanager/authentication/controller/TaskController.java
@@ -10,7 +10,10 @@ import org.springframework.web.bind.annotation.PutMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestParam;
 import io.taskmanager.authentication.service.TaskService;
+import org.springframework.web.bind.annotation.*;
 
+@RestController
+@RequestMapping("/api/v1/teams")
 public class TaskController {
     private final TaskService taskService;
 
